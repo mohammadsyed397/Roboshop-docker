3967f5a76fa259a2eacdd00e7a1ce056c1b31f30
mohammadsyed397 <mohammad.syed601@gmail.com>
2025-08-24 14:20:10 -0400

    latest

--- mysql/db/master-data.sql
+++ mysql/db/master-data.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fb4e339f2b5ec91a24e6b873e4e02f8d7b5b7a222d812ac43b49d71f17f2fd9
-size 64581448
+oid sha256:aeab5c03adb30119c4245098afa1a46c13577c2772afa4e1f38c02212eb339b5
+size 92
