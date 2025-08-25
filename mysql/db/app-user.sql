e78b43c23a350f1a2aca6132566aac2844a175a7
Mohammad Syed <52034679+mohammadsyed397@users.noreply.github.com>
2025-08-25 11:42:39 -0400

    Delete mysql directory

--- mysql/db/app-user.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2e4bf8f84ad636f72113e6ffb06d97522f5daa8e10b687cb6ad2b4f0b4e926f3
-size 156
--- mysql/db/master-data.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2fb4e339f2b5ec91a24e6b873e4e02f8d7b5b7a222d812ac43b49d71f17f2fd9
-size 64581448
